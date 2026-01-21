32bc54a7a129528c7acfc4d074979b423f836812
Wayne Gakuo <waynejotham@gmail.com>
2026-01-21 07:45:42 +0300

    refactor(services): move vehicle interfaces to a dedicated model file
    Relocated `Vehicle` and `VehicleResponse` interfaces from the vehicle service to a new `vehicle.model.ts` file to improve code organization and reuse.

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -1,31 +1,6 @@
 import {computed, Injectable, signal} from '@angular/core';
-import {httpResource, HttpResponse} from '@angular/common/http';
-
-export interface Vehicle {
-  name: string;
-  model: string;
-  manufacturer: string;
-  cost_in_credits: string;
-  length: string;
-  max_atmosphering_speed: string;
-  crew: string;
-  passengers: string;
-  cargo_capacity: string;
-  consumables: string;
-  vehicle_class: string;
-  pilots: string[];
-  films: string[];
-  created: string;
-  edited: string;
-  url: string;
-}
-
-export interface VehicleResponse {
-  count: number;
-  next: string;
-  previous: string;
-  results: Vehicle[];
-}
+import {httpResource} from '@angular/common/http';
+import {Vehicle, VehicleResponse} from '../models/vehicle.model';
 
 @Injectable({
   providedIn: 'root',
