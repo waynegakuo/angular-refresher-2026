8dd9a5a9815b6cb41d99d15b9114b5cc868ec4c9
Wayne Gakuo <waynejotham@gmail.com>
2026-04-13 16:38:33 +0300

    refactor(child): remove unused `newTitle` and `valueChanged` properties

--- src/app/components/child/child.ts
+++ src/app/components/child/child.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, input, output, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, input, Output } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 @Component({
@@ -12,17 +12,10 @@ export class Child {
   title = input<string>('');
 
   updatedTitle: string = '';
-  newTitle: string = '';
-
-  valueChanged = output<string>();
 
   @Output() titleChange = new EventEmitter<string>();
 
   changeTitle() {
     this.titleChange.emit(this.updatedTitle);
-  }
-
-  updateTitle() {
-    this.valueChanged.emit(this.newTitle);
   }
 }
